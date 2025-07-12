d72adc35b6a5c2ac5d03fa0784c868c0f2a982f9
Jacob Filek <jacob.filek@gmail.com>
2025-07-12 09:26:56 -0700

    Initial Commit

---
+++ ServiceItTests/ServiceItTests.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceItTests.swift
+//  ServiceItTests
+//
+//  Created by Jacob Filek on 7/12/25.
+//
+
+import Testing
+@testable import ServiceIt
+
+struct ServiceItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
